f046787397452e58776e632c284101c8474598f0
thiagomeneguzzi <thiagonmeneguzzi@gmail.com>
2021-11-28 14:25:59 -0300

    created Cadastro-usuario component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
+
+
 
 @NgModule({
   declarations: [
@@ -17,7 +24,12 @@ import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.co
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
