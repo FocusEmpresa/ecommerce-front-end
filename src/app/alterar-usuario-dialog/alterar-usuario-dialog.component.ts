644aa64803d2d14843b71225f8147ea6282b70a7
LeonardoGarciah <leonardoformulaum@Hotmail.com>
2021-12-04 10:44:18 -0300

    Using ViaCep API to auto fill in address fields

--- src/app/alterar-usuario-dialog/alterar-usuario-dialog.component.ts
+++ src/app/alterar-usuario-dialog/alterar-usuario-dialog.component.ts
@@ -3,13 +3,68 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-alterar-usuario-dialog',
   templateUrl: './alterar-usuario-dialog.component.html',
-  styleUrls: ['./alterar-usuario-dialog.component.css']
+  styleUrls: ['./alterar-usuario-dialog.component.css'],
 })
 export class AlterarUsuarioDialogComponent implements OnInit {
+  cep: any;
 
-  constructor() { }
+  rua: String = '';
+  bairro: String = '';
+  cidade: String = '';
+  uf: String = '';
 
-  ngOnInit(): void {
+  constructor() {}
+  onBlurMethod() {
+    this.pesquisacep(this.cep);
   }
 
+  encontrado: boolean = false;
+
+  meu_callback(conteudo: any) {
+    fetch(conteudo)
+      .then((response) => response.json())
+      .then((result) => {
+        this.rua = result.logradouro;
+        this.bairro = result.bairro;
+        this.cidade = result.localidade;
+        this.uf = result.uf;
+        console.log(this.rua);
+      })
+      .catch((err) => {
+        alert('CEP inválido!!!');
+      });
+  }
+
+  pesquisacep(valor: String) {
+    //Nova variável "cep" somente com dígitos.
+    var cep = '';
+    if (valor != undefined) {
+      cep = valor.replace(/\D/g, '');
+    }
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != '') {
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+        this.encontrado = true;
+        //Preenche os campos com "..." enquanto consulta webservice.
+        this.rua = '...';
+        this.bairro = '...';
+        this.cidade = '...';
+
+        //Sincroniza com o callback.
+        const url = 'https://viacep.com.br/ws/' + cep + '/json/';
+        this.meu_callback(url);
+      } //end if.
+      else {
+        //cep é inválido.
+        alert('Formato de CEP inválido.');
+      }
+    } //end if.
+  }
+
+  ngOnInit(): void {}
 }
